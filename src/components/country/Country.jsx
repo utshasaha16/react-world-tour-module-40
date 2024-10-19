6002a39bbd2a0c67b7c142e0381edb38ebff5cef
utshasaha16 <utsharoy12345@gmail.com>
2024-10-19 21:01:42 +0600

    display country name and flags

---
+++ src/components/country/Country.jsx
@@ -0,0 +1,13 @@
+import './country.css'
+const Country = ({country}) => {
+    console.log(country);
+    const {name, flags} = country;
+    return (
+        <div className='country'>
+            <h3>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
